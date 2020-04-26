571cde81a00f415cfe97b583931e544f5e1a8c2b
V <22565619+aluminiumi@users.noreply.github.com>
2020-04-26 07:33:12 -0400

    added support for websocket communication

--- index.js
+++ index.js
@@ -1,13 +1,180 @@
+// WebSocket code based on code from https://gist.github.com/martinsik/2031681
+
+"use strict";
+
 const express = require('express')
 const serveIndex = require('serve-index');
 const app = express()
+const webSocketServer = require('websocket').server;
+const http = require('http');
+
 const port = 42001
+const webSocketsServerPort = 13347; // port websocket server listens on
+
+// global variables
+var history = [ ]; // latest 100 messages
+var clients = [ ]; // list of currently connected clients (users)
 
 app.use('/', serveIndex(__dirname + '/wasm'));
 app.use(express.static(__dirname + "/wasm"))
 
 app.listen(port, () => {
-    console.log(`server listening on port ${port}!`)
-    console.log(`check http:\/\/localhost:42001/`)
+    dateLog(`web server listening on port ${port}; check http:\/\/localhost:${port}/`)
 })
+
+// helper function for escaping input strings
+function htmlEntities(str) {
+    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;')
+                      .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
+
+function dateLog(str) {
+    console.log((new Date()) + " " + str);
+}
+
+// start HTTP server
+var server = http.createServer(function(request, response) {
+    // do nothing but listen with HTTP server
+    // websocket will handle requests    
+}).listen(webSocketsServerPort, function() {
+    dateLog("websocket server is listening on port " + webSocketsServerPort);
+});
+    
+
+// start WebSocket server
+var wsServer = new webSocketServer({
+    // WebSocket server is tied to a HTTP server. WebSocket request is just
+    // an enhanced HTTP request. For more info http://tools.ietf.org/html/rfc6455#page-6
+    httpServer: server
+});
+
+// called when request received by WebSocket server
+wsServer.on('request', function(request) {
+
+    var connection = request.accept(null, request.origin); 
+    var index = clients.push(connection) - 1;
+    var userName = false;
+    var remoteAddr = connection.remoteAddress
+    var origin = request.origin
+
+    dateLog('Accepted connection from '+remoteAddr+' (origin ' + origin+')');
+
+    // send back chat history
+    if (history.length > 0) {
+        connection.sendUTF(JSON.stringify( { type: 'history', data: history} ));
+    }
+
+    // user sent some message
+    connection.on('message', function(message) {
+        if (message.type !== 'utf8') { // accept only text
+            return;
+        }
+
+        if (userName === false) { // first message sent by user is their name
+            userName = htmlEntities(message.utf8Data);
+            connection.sendUTF(JSON.stringify({ type:'ok', id:index })); // tell them their index
+            dateLog('User from '+remoteAddr+' is known as: ' + userName);
+            return;
+        }
+
+        dateLog('Received message from '+ userName + ': ' + message.utf8Data);
+                
+        // keep history of all sent messages
+        var msgObj = logMessage(userName, message);
+
+        // process the command if it's a command
+        var firstWord = getFirstWord(msgObj.text);
+        if (firstWord == 'command') {
+            processCommand(msgObj);
+            return;
+        }
+
+        // broadcast message to all connected clients
+        broadcastMessage(msgObj);
+    });
+
+    function processCommand(msgObj) {
+        var text = msgObj.text;
+        var command = text.split(" ");
+        var argCount = command.length-1;
+        dateLog("argcount: "+argCount);
+        if (argCount == 0) {
+            sendCommandHelp(index);
+            return;
+        }
+        if (argCount == 2) {
+            if (command[2] === 'sayhi') {
+                dateLog("Directing "+command[1]+" to SAYHI");
+                sendSayHi(command[1]);
+            }
+        }
+    }
+
+    function getFirstWord(msgText) {
+        return msgText.split(" ", 1)
+    }
+
+    function sendSayHi(destination_index) {
+        var data = "sayhi";
+
+        var obj = {
+            time: (new Date()).getTime(),
+            text: htmlEntities(data),
+            author: "cnc",
+        };
+
+        singlecastCommand(obj, destination_index);
+    }
+
+    function sendCommandHelp(destination_index) {
+        var data = "Commands:\n" 
+                 + " { <source> sayhi }\n";
+
+        var obj = {
+            time: (new Date()).getTime(),
+            text: htmlEntities(data),
+            author: "cnc",
+        };
+
+        singlecastMessage(obj, index);
+    }
+
+    function singlecastCommand(msgObj, destination) {
+        var json = JSON.stringify({ type:'command', data: msgObj });
+        clients[destination].sendUTF(json);
+    }
+
+    function singlecastMessage(msgObj, destination) {
+        var json = JSON.stringify({ type:'message', data: msgObj });
+        clients[destination].sendUTF(json);
+    }
+
+    function broadcastMessage(msgObj) {
+        var json = JSON.stringify({ type:'message', data: msgObj });
+        for (var i=0; i < clients.length; i++) {
+            clients[i].sendUTF(json);
+        }
+    }
+
+    function logMessage(userName, message) {
+        var obj = {
+            time: (new Date()).getTime(),
+            text: htmlEntities(message.utf8Data),
+            //author: userName,
+            author: index,
+        };
+        history.push(obj);
+        history = history.slice(-100);
+        return obj
+    }    
+
+    // user disconnected
+    connection.on('close', function(connection) {
+        if (userName !== false) {
+            dateLog("Peer "+ userName + " ("+remoteAddr+") disconnected.");
+            clients.splice(index, 1); // remove user from the list of connected clients
+        }
+    });
+
+});
 
---
+++ wasm/commandandcontrol.js
@@ -0,0 +1,108 @@
+// Based on code from https://gist.github.com/martinsik/2031681
+
+$(function () {
+    "use strict";
+
+    var server_host = "127.0.0.1"
+    var server_port = "13347"
+    var max_node_number = 60000
+
+    var content = $('#content');
+    var input = $('#input');
+    var status = $('#status');
+
+    // identity sent to the server
+    var myNumber = Math.floor(Math.random() * max_node_number);
+
+    // use whichever WebSocket implementation we can find
+    window.WebSocket = window.WebSocket || window.MozWebSocket;
+
+    // if browser doesn't support WebSocket, just show some notification and exit
+    if (!window.WebSocket) {
+        content.html($('<p>', { text: 'WebSocket support not found.' } ));
+        input.hide();
+        $('span').hide();
+        return;
+    }
+
+    // open connection
+    var connection = new WebSocket('ws://'+server_host+':'+server_port);
+
+    // register identity at startup
+    connection.onopen = function () {
+        status.text(myNumber + ': ');
+        connection.send(myNumber);
+    };
+
+    // print error if error occurs
+    connection.onerror = function (error) {
+        content.html($('<p>', { text: 'Connection error' } ));
+    };
+
+    // handle incoming messages from server
+    connection.onmessage = function (message) {
+        try {
+            var json = JSON.parse(message.data);
+        } catch (e) {
+            console.log('Invalid JSON: ', message.data);
+            return;
+        }
+
+        switch(json.type) {
+        case 'ok': // first server response; acknowledges registration
+            myNumber = json.id
+            status.text(myNumber + ': ');
+            input.removeAttr('disabled').val('').focus();
+            break;
+
+        case 'history': // entire message history
+            // insert each message into the chat window
+            for (var i=0; i < json.data.length; i++) {
+                addMessage(json.data[i].author, json.data[i].text, new Date(json.data[i].time));
+            }
+            break;
+
+        case 'message':
+            input.removeAttr('disabled'); // let the user write another message
+            addMessage(json.data.author, json.data.text);
+            break;
+
+        case 'command':
+            addMessage(json.data.author, "COMMAND RECEIVED: "+json.data.text);
+            break;
+
+        default:
+            console.log('Unrecognized response from server', json);
+        }
+    };
+
+    // send message when user presses Enter key
+    input.keydown(function(e) {
+        if (e.keyCode === 13) { // enter key
+            var msg = $(this).val();
+            if (!msg) {
+                return;
+            }
+
+            // send the message
+            connection.send(msg);
+
+            // disable the input field until server sends back response
+            input.attr('disabled', 'disabled').val('');
+
+        }
+    });
+
+    // print error message if 3 seconds pass without server response
+    setInterval(function() {
+        if (connection.readyState !== 1) {
+            status.text('Error');
+            input.attr('disabled', 'disabled').val('Connection failed.');
+        }
+    }, 3000);
+
+    // add message to the chat window
+    function addMessage(author, message) {
+        content.prepend('<p><span>' + author + '</span>: ' + message + '</p>');
+    }
+});
